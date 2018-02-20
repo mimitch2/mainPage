463be2628afe97c35e5894c1c7fadbdb679e6f15
mimitch2 <mike@melodix.com>
2018-02-20 17:04:48 -0600

    checking if goDady works

--- js/script.js
+++ js/script.js
@@ -1,32 +1,62 @@
 'use strict';
 // document.addEventListener("DOMContentLoaded", function(event) {
 window.onload = function() {
-
-// --------> just for turning on/off ratio graphoc, design tool only
-
-  $(".LS").click(function(){
-$('#ratio').toggle();
-});
-
-$(".P").click(function(){
-$('#ratioRotated').toggle();
-});
-
-// --------------------> END
-
   $(document).ready(function() {
-$('#mainLogo').animate({marginLeft: '90px', opacity: '1.0'}, 1300);
+    // --------> just for turning on/off ratio graphoc, design tool only
 
-$('footer').animate({marginLeft: '0px', opacity: '1.0'}, 1300);
+    $(".LS").click(function() {
+      $('#ratio').toggle();
+    });
 
-// $(window).scroll(function(){
-// if ($(window).scrollTop()>50) {
-//
-//   $('#mainLogo').animate({marginTop: '100px'})
-// } else {
-//   $('#mainLogo').animate({marginTop: '0px'})
-// }
-// });
+    $(".P").click(function() {
+      $('#ratioRotated').toggle();
+    });
+
+    // --------------------> END
+
+    const mq = window.matchMedia("(min-width: 768px)");
+
+    if (mq.matches) {
+      $('#mainLogo').animate({
+        marginLeft: '70px',
+        opacity: '1.0'
+      }, 1300);
+      $('footer').animate({
+        marginLeft: '0px',
+        marginBottom: '0px',
+        opacity: '1.0'
+      }, 1300);
+      $('nav').animate({
+        marginTop: '110px',
+        opacity: '1.0'
+      }, 1300);
+
+    } else {
+      $('#mainLogo').animate({
+        marginLeft: '1.0em',
+        opacity: '1.0'
+      }, 1300);
+      $('footer').animate({
+        marginBottom: '0px',
+        marginLeft: '0px',
+        opacity: '1.0'
+      }, 1300);
+      $('nav').animate({
+        marginTop: '70px',
+        opacity: '1.0'
+      }, 1300);
+
+    }
+
+    // 
+    // $(window).scroll(function(){
+    // if ($(window).scrollTop()>50) {
+    //
+    //   $('#mainLogo').animate({marginTop: '100px'})
+    // } else {
+    //   $('#mainLogo').animate({marginTop: '0px'})
+    // }
+    // });
 
 
 
@@ -36,13 +66,7 @@ $('footer').animate({marginLeft: '0px', opacity: '1.0'}, 1300);
 
 
 
+  }); //document ready close
 
-
-
-
-
-
-});//document ready close
-
-}//window onload close
+} //window onload close
 // });
